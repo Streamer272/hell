d44ee72a4e0354dcebae0e6c07f80cace8ce01c9
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-04-22 07:12:39 +0200

    :zap: Adds login text

--- app/src/main/java/com/streamer272/hell/LoginActivity.kt
+++ app/src/main/java/com/streamer272/hell/LoginActivity.kt
@@ -1,8 +1,26 @@
 package com.streamer272.hell
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginActivity(changeState: (AppState) -> Unit) {
-    TODO()
+    var username by remember { mutableStateOf("") }
+
+    Box {
+        Text(text = "Log in", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), fontSize = 40.sp)
+        TextField(
+            value = "",
+            onValueChange = fun (newUsername: String) {
+                username = newUsername
+            },
+            label = { Text(text = "Username") },
+        )
+    }
 }
