6c88c288fc094c81cf1609aaf3e308588b2568a9
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-04-23 10:54:06 +0200

    :zap: Adds localization

--- app/src/main/java/com/streamer272/hell/ui/login/LoginActivity.kt
+++ app/src/main/java/com/streamer272/hell/ui/login/LoginActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.streamer272.hell.AppState
+import com.streamer272.hell.R
 import io.ktor.http.*
 import kotlinx.coroutines.launch
 
@@ -21,15 +23,22 @@ import kotlinx.coroutines.launch
 fun LoginActivity(changeState: (AppState) -> Unit) {
     val context = LocalContext.current
     val composableScope = rememberCoroutineScope()
+    val toastUsernameMissing = stringResource(R.string.toast_username_missing)
+    val toastPasswordMissing = stringResource(R.string.toast_password_missing)
+    val toastLoginSuccessful = stringResource(R.string.toast_login_successful)
+    val toastLoginFailed = stringResource(R.string.toast_login_failed)
+    val toastCredsInvalid = stringResource(R.string.toast_creds_invalid)
+    val toastError = stringResource(R.string.toast_error)
+
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
     val login = fun() {
         if (username == "") {
-            return Toast.makeText(context, "Username is required", Toast.LENGTH_SHORT).show()
+            return Toast.makeText(context, toastUsernameMissing, Toast.LENGTH_SHORT).show()
         }
         else if (password == "") {
-            return Toast.makeText(context, "Password is required", Toast.LENGTH_SHORT).show()
+            return Toast.makeText(context, toastPasswordMissing, Toast.LENGTH_SHORT).show()
         }
 
         composableScope.launch {
@@ -37,18 +46,18 @@ fun LoginActivity(changeState: (AppState) -> Unit) {
                 val result = login(username, password)
                 when (result.status) {
                     HttpStatusCode.OK -> {
-                        Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, toastLoginSuccessful, Toast.LENGTH_SHORT).show()
                         changeState(AppState.DASHBOARD)
                     }
                     HttpStatusCode.Unauthorized -> {
-                        Toast.makeText(context, "Invalid username or password", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, toastCredsInvalid, Toast.LENGTH_SHORT).show()
                     }
                     else -> {
-                        Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, toastLoginFailed, Toast.LENGTH_SHORT).show()
                     }
                 }
             } catch (e: Exception) {
-                Toast.makeText(context, "Something went wrong: ${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, toastError.replace("\$ERR", "${e.message}"), Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -58,7 +67,7 @@ fun LoginActivity(changeState: (AppState) -> Unit) {
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Text(text = "Log in", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), fontSize = 40.sp)
+        Text(text = stringResource(R.string.text_log_in), textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), fontSize = 40.sp)
 
         Column(
             modifier = Modifier.padding(bottom = 48.dp)
@@ -68,7 +77,7 @@ fun LoginActivity(changeState: (AppState) -> Unit) {
                 onValueChange = fun (newUsername: String) {
                     username = newUsername
                 },
-                label = { Text(text = "Username") },
+                label = { Text(text = stringResource(R.string.text_username)) },
                 modifier = Modifier
                     .padding(top = 32.dp)
                     .align(Alignment.CenterHorizontally)
@@ -80,7 +89,7 @@ fun LoginActivity(changeState: (AppState) -> Unit) {
                 },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 visualTransformation = PasswordVisualTransformation(),
-                label = { Text(text = "Password") },
+                label = { Text(text = stringResource(R.string.text_password)) },
                 modifier = Modifier
                     .padding(top = 8.dp)
                     .align(Alignment.CenterHorizontally)
@@ -91,7 +100,7 @@ fun LoginActivity(changeState: (AppState) -> Unit) {
                     .padding(top = 16.dp)
                     .align(Alignment.End)
             ) {
-                Text(text = "Log in", fontSize = 18.sp)
+                Text(text = stringResource(R.string.text_log_in), fontSize = 18.sp)
             }
         }
     }
